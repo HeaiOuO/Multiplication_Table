054e82554d6c5bcad7ad6b78fb022d7fa002debc
He Ai <heai0210@gmail.com>
2019-04-01 15:35:42 -0700

    multi_table

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace multiplicationTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type a integer number");
+            int n = int.Parse(Console.ReadLine());
+
+            // Declaring variables to keep track of the position of the number to print in the matrix
+            int row;
+            int col;
+
+            // Looping and multiplicating each row by each column.                       
+            for(row=1;row<=n;row++)
+            {
+                for(col=1;col<=n;col++){
+                    Console.Write(row*col);
+                    if(col<n){
+                        Console.Write(" ");
+                    }
+                    if((row*col)<10){
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
